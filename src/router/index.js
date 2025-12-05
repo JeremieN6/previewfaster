804b8f7491ad542bb457d61213643070cb12c5bb
JeremieN6 <ngoyi.jeremie@gmail.com>
2025-12-05 18:28:33 +0100

    feat: (Version propre) suppression/nettoyage des composants et templates inutilisés dans ce projet

--- src/router/index.js
+++ src/router/index.js
@@ -3,7 +3,6 @@ import { createRouter, createWebHistory } from 'vue-router'
 const LandingPage = () => import('../pages/LandingPage.vue')
 const BuilderPage = () => import('../pages/Builder.vue')
 const BuilderManualPage = () => import('../pages/BuilderManual.vue')
-const BuilderCatalogPage = () => import('../pages/BuilderCatalog.vue')
 
 const router = createRouter({
   history: createWebHistory(),
@@ -22,11 +21,6 @@ const router = createRouter({
       path: '/builder-manual',
       name: 'builder-manual',
       component: BuilderManualPage
-    },
-    {
-      path: '/templates',
-      name: 'builder-catalog',
-      component: BuilderCatalogPage
     },
     {
       path: '/:pathMatch(.*)*',
